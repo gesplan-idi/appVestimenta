2985b7a2db9f4edcbc90a59b139acf42a6a81dc2
jrodmac <jrodmac@gesplan.es>
2025-11-07 13:27:40 +0000

    eliminamos codigo sobrante

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,5 @@
 import { Routes } from '@angular/router';
-
 import { ConfigurarComponent } from './inicio/configurar/configurar.component';
-import { PuestosComponent } from './puestos/puestos/puestos.component';
 import { UsuariosComponent } from './usuarios/usuarios.component';
 
 export const routes: Routes = [
@@ -9,10 +7,7 @@ export const routes: Routes = [
     path: 'inicio/configurar',
     component: ConfigurarComponent
   },
-   {
-    path: 'puestos/puestos',
-    component: PuestosComponent
-  },
+   
    {
     path: 'usuarios/usuarios',
     component: UsuariosComponent
